9f0400e25e5c3103447a30e52b4ae287c48a1e80
John McCombs <internal+denys@coredevelop.com>
2021-02-17 18:35:54 +0200

    add observer

---
+++ app/code/Overdose/LessonTwo/Observer/ControllerActionDie.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Overdose\LessonTwo\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+class ControllerActionDie implements ObserverInterface
+{
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+	die("keke");
+    }
+}
